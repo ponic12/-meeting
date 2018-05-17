71f0d2bff1d33c55dbec46a1c787679186d93ece
pyp <pyp@github.com>
2018-05-17 10:49:59 -0300

    fix

--- code/src/shared/components/hours/hours.component.ts
+++ code/src/shared/components/hours/hours.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Input, Output, EventEmitter, NgZone } from '@angular/core'
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core'
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms'
 import { NgStyle } from '@angular/common'
 
@@ -11,9 +11,11 @@ import { ApplicationService } from '../../services/application.service'
 })
 export class HoursComponent implements OnInit, OnDestroy {
    @Input() dictionary: any = []
+   @Input('max') maxVal: number = 0
+   @Input() editUser :string
+   @Output() selectedCell: EventEmitter<string[]> = new EventEmitter<string[]>()
 
    constructor(
-      private zone: NgZone,
       private globalSrv: GlobalService,
       private appSrv: ApplicationService
    ) {
@@ -28,8 +30,15 @@ export class HoursComponent implements OnInit, OnDestroy {
       console.log('HoursComponent init')
    }
 
-   toggleHour(hr) {
-      this.zone.run(() => { hr.value = 1 - hr.value })
+   selectCell(hr){
+      if ((hr.members.length == 1)&& (hr.members[0].username == this.editUser))
+         hr.value = 1 - hr.value
+      else{
+         this.selectedCell.emit(hr.members)
+      }
    }
 
+   calculateOpacity(val){
+      return (val/this.maxVal)
+   }
 }
