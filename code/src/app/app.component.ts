da136c82f912d4e1960a9748d4ad3036f3e8fb21
pyp <pyp@github.com>
2018-07-20 22:29:12 -0300

    fix

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy, NgZone, ViewChild } from '@angular/core'
-import { Platform, Events, NavController } from 'ionic-angular'
-import { AngularFirestore } from 'angularfire2/firestore'
+import { Platform, NavController } from 'ionic-angular'
 import { StatusBar } from '@ionic-native/status-bar'
 import { SplashScreen } from '@ionic-native/splash-screen'
+import { AngularFirestore } from 'angularfire2/firestore'
 import { CodePush, SyncStatus } from '@ionic-native/code-push'
 import { ApplicationService } from 'fwk-services'
 
@@ -11,61 +11,82 @@ import { ApplicationService } from 'fwk-services'
 })
 export class MeetingApp implements OnInit, OnDestroy {
    @ViewChild('content') nav: NavController
-   progressStatus: string = ""
+   prog: number = 0
+   showProgressBar:boolean=false
 
    constructor(
+      private zone:NgZone,
       private codePush: CodePush,
       private afs: AngularFirestore,
       private appSrv:ApplicationService,
-      platform: Platform,
-      statusBar: StatusBar,
-      splashScreen: SplashScreen
+      private platform: Platform,
+      private statusBar: StatusBar,
+      private splashScreen: SplashScreen
    ) {
       console.log('MeetingApp constructor');
       this.afs.firestore.settings({ timestampsInSnapshots: true })
       this.afs.firestore.enablePersistence()
 
-      platform.ready().then(() => {
-         this.codePush.sync({}, (progress) => {
-            this.progressStatus = JSON.stringify(progress)
-         }).subscribe(status => {
-            switch (status) {
-               case SyncStatus.CHECKING_FOR_UPDATE:
-                  this.appSrv.message('checking for update', '')
-                  break;
-               case SyncStatus.AWAITING_USER_ACTION:
-                  this.appSrv.basicAlert('waiting for user input')
-                  break;
-               case SyncStatus.IN_PROGRESS:
-                  this.appSrv.message('update in progress')
-                  break;
-               case SyncStatus.DOWNLOADING_PACKAGE:
-                  this.appSrv.message('downloading package')
-                  break;
-               case SyncStatus.UP_TO_DATE:
-                  this.appSrv.message('app up to date')
-                  break;
-               case SyncStatus.INSTALLING_UPDATE:
-                  this.appSrv.message('installing update')
-                  break;
-               case SyncStatus.UPDATE_IGNORED:
-                  this.appSrv.message('update ignored')
-                  break;
-               case SyncStatus.UPDATE_INSTALLED:
-                  this.appSrv.message('update installed')
-                  setTimeout(() => {
-                     window.location.reload();
-                  }, 1000);
-               case SyncStatus.ERROR:
-                  this.appSrv.basicAlert('an error occurred')
-                  break;
-            }
-         })
+      this.platform.ready().then(() => {
+         console.log('platform ready....')
+         setTimeout(() => {
+            console.log('starting update validation......')
+            this.codePush.sync({}, (progress) => {
+               this.zone.run(()=>{
+                  this.prog = Math.floor(progress.receivedBytes / progress.totalBytes * 100)
+                  console.log('progress: ', this.prog)
+               })     
+            }).subscribe(status => {
+               switch (status) {
+                  case SyncStatus.CHECKING_FOR_UPDATE:
+                     console.log('CHECKING_FOR_UPDATE: ', SyncStatus.CHECKING_FOR_UPDATE)
+                     this.appSrv.message('checking for update', '')
+                     break;
+                  case SyncStatus.AWAITING_USER_ACTION:
+                     console.log('AWAITING_USER_ACTION: ', SyncStatus.AWAITING_USER_ACTION)
+                     this.appSrv.basicAlert('waiting for user input')
+                     break;
+                  case SyncStatus.IN_PROGRESS:
+                     console.log('IN_PROGRESS: ', SyncStatus.IN_PROGRESS)
+                     this.appSrv.message('update in progress')
+                     break;
+                  case SyncStatus.DOWNLOADING_PACKAGE:
+                     console.log('DOWNLOADING_PACKAGE: ', SyncStatus.DOWNLOADING_PACKAGE)                  
+                     this.appSrv.message('downloading package')
+                     this.zone.run(()=>{
+                        this.showProgressBar = true
+                        console.log('showProgressBar: ', this.showProgressBar)
+                     })
+                     break;
+                  case SyncStatus.UP_TO_DATE:
+                     console.log('UP_TO_DATE: ', SyncStatus.UP_TO_DATE)
+                     this.appSrv.message('app up to date')
+                     break;
+                  case SyncStatus.INSTALLING_UPDATE:
+                     console.log('INSTALLING_UPDATE: ', SyncStatus.INSTALLING_UPDATE)
+                     this.appSrv.message('installing update')
+                     break;
+                  case SyncStatus.UPDATE_IGNORED:
+                     console.log('UPDATE_IGNORED: ', SyncStatus.UPDATE_IGNORED)
+                     this.appSrv.message('update ignored')
+                     break;
+                  case SyncStatus.UPDATE_INSTALLED:
+                     console.log('UPDATE_INSTALLED: ', SyncStatus.UPDATE_INSTALLED)
+                     this.codePush.restartApplication();
+                     this.appSrv.basicAlert('update installed, restart app!')
+                  case SyncStatus.ERROR:
+                     this.appSrv.message('SyncStatus.ERROR....')
+                     console.log('SyncStatus.ERROR: ', SyncStatus.ERROR)
+                     break;
+               }
+            })
+         }, 3000);
+
          // Okay, so the platform is ready and our plugins are available.
          // Here you can do any higher level native things you might need.
-         statusBar.styleDefault()
+         this.statusBar.styleDefault()
          //statusBar.styleBlackOpaque()
-         splashScreen.hide()
+         this.splashScreen.hide()
       }).catch(err => {
          console.error(err)
          this.appSrv.basicAlert(err, 'Error!')
--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, ErrorHandler } from '@angular/core'
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { SplashScreen } from '@ionic-native/splash-screen'
 import { StatusBar } from '@ionic-native/status-bar'
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular'
+
 import { Contacts } from '@ionic-native/contacts'
 import { Facebook } from '@ionic-native/facebook'
 import { CodePush } from '@ionic-native/code-push'
@@ -16,6 +17,7 @@ import { AngularFireModule } from 'angularfire2'
 import { AngularFirestoreModule } from 'angularfire2/firestore'
 import { FirebaseService } from '../shared/services/firebase.service'
 import { FIREBASE_CONFIG } from '../shared/services/firebase.config'
+
 
 @NgModule({
    declarations: [
